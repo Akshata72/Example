bd249fbf42b5c510a0c41a9e7168a14c2653c288
Akshata72 <sableakshata880@gmail.com>
2022-03-11 08:26:39 +0530

    Added:UC1-SrqureRoot_Of_Num

---
+++ Example/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Example
+{
+    class program
+    {
+        
+        static void Main(string[]args)
+        {
+            int X = 20;
+            
+            double Srqt = (Math.Sqrt(X));
+            Console.WriteLine("This is a SquerRoot of 20:"+ Srqt);
+        }
+    }
+}
